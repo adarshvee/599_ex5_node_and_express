2a78409104e92b48f9e652583c6968e1bea2ccd2
Adarsh Vijayaraghavan <adarshvijayaraghavan@gmail.com>
2018-04-14 22:51:09 -0400

    Add App directory to repo

---
+++ app/routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  var dataFile = req.app.get('appData');
+  var pagePhotos = [];
+  var pageHighlights = dataFile.data.highlights;
+
+  pageHighlights.forEach(function(item) {
+    pagePhotos = pagePhotos.concat(item.artwork);
+  });
+
+  res.render('index', {
+    pageTitle: 'Home',
+    artwork: pagePhotos,
+    highlights: pageHighlights,
+    pageID: 'home'
+  });
+
+});
+
+module.exports = router;
